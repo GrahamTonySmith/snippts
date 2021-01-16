b617d4af776ab9778d44c4ad05179ead9c5ae4ea
Graham Smith <grahamtonysmith@protonmail.com>
2021-01-16 22:06:33 +0000

    Add mediator design pattern

---
+++ mediator.py
@@ -0,0 +1,77 @@
+from typing import Protocol
+
+
+class PMediator(Protocol):
+
+    def notify(self, sender: object, event: str) -> None: ...
+
+
+class BaseComponent:
+
+    # public because we need to allow for a setter
+    mediator: PMediator
+
+    def __init__(self, mediator: PMediator = None) -> None:
+        self.mediator = mediator
+
+
+class ComponentOne(BaseComponent):
+
+    def do_a(self) -> None:
+        print("Component One does A.")
+        self.mediator.notify(self, "A")
+
+    def do_b(self) -> None:
+        print("Component One does B.")
+        self.mediator.notify(self, "B")
+
+
+class ComponentTwo(BaseComponent):
+
+    def do_c(self) -> None:
+        print("Component Two does C.")
+        self.mediator.notify(self, "C")
+
+    def do_d(self) -> None:
+        print("Component Two does D.")
+        self.mediator.notify(self, "D")
+
+
+class ConcreteMediator(PMediator):
+
+    _component_one: ComponentOne
+    _component_two: ComponentTwo
+
+    def __init__(self, component_one: ComponentOne, component_two: ComponentTwo) -> None:
+        self._component_one = component_one
+        self._component_one.mediator = self
+        self._component_two = component_two
+        self._component_two.mediator = self
+
+    def notify(self, sender: object, event: str) -> None:
+        if event == 'A':
+            print('ConcreteMediator reacts on A and triggers the following operations')
+            self._component_two.do_c()
+        if event == 'D':
+            print('ConcreteMediator reacts on A and triggers the following operations')
+            self._component_one.do_b()
+            self._component_two.do_c()
+
+
+def main():
+    # The client code.
+    c1 = ComponentOne()
+    c2 = ComponentTwo()
+    mediator = ConcreteMediator(c1, c2)
+
+    print("Client triggers operation A.")
+    c1.do_a()
+
+    print("\n", end="")
+
+    print("Client triggers operation D.")
+    c2.do_d()
+
+
+if __name__ == "__main__":
+    main()
