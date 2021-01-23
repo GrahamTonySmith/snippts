8ac9af035ef33bf995b7be9608ee25c604a64178
Graham Smith <grahamtonysmith@protonmail.com>
2021-01-23 06:32:57 +0000

    Add experiments with mediators

---
+++ mediator.register.py
@@ -0,0 +1,57 @@
+from typing import Protocol, Set
+
+
+class PMediator(Protocol):
+
+    def notify(self, msg: str) -> None: ...
+    """notify the mediator"""
+
+    def register(self, component) -> None: ...
+    """register a component with the mediator"""
+
+
+class BaseComponent:
+
+    def __init__(self, name: str, mediator: PMediator = None):
+        self.name: str = name
+        self.mediator: PMediator = mediator
+
+    def handle(self, msg):
+        """handle a msg send from the mediator"""
+        print(f'{self.name} handled msg: {msg}')
+
+    def notify(self, msg):
+        """notify the mediator of a msg"""
+        self.mediator.notify(msg)
+
+
+class Mediator(PMediator):
+
+    components: Set[BaseComponent] = set()
+
+    def register(self, component: BaseComponent):
+        component.mediator = self
+        self.components.add(component)
+
+    def notify(self, msg: str):
+        """notify the components by pushing the msg to handle"""
+        for component in self.components:
+            component.handle(msg)
+
+
+def main():
+
+    components = [
+        BaseComponent(f'component {letter}')
+        for letter
+        in ('A', 'B', 'C')
+    ]
+
+    mediator = Mediator()
+    for component in components: mediator.register(component)
+
+    component_a, *rest = components
+    component_a.notify(msg='Hello from A')
+
+if __name__ == '__main__':
+    main()
